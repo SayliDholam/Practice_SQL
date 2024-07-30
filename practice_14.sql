d691b7cb1851df85c31152cc9b28fa0e31f22ebc
Sayli R Dholam <saylidholam08@gmail.com>
2024-07-31 00:52:32 +0530

    Add files via upload

---
+++ practice_14.sql
@@ -0,0 +1,129 @@
+SELECT COUNT(low) AS low
+  FROM tutorial.aapl_historical_stock_price
+
+-- 
+
+SELECT COUNT(year) AS year,
+       COUNT(month) AS month,
+       COUNT(open) AS open,
+       COUNT(high) AS high,
+       COUNT(low) AS low,
+       COUNT(close) AS close,
+       COUNT(volume) AS volume
+  FROM tutorial.aapl_historical_stock_price
+
+--
+
+SELECT SUM(open)/COUNT(open) AS avg_open_price
+  FROM tutorial.aapl_historical_stock_price
+
+--
+
+SELECT MIN(low)
+  FROM tutorial.aapl_historical_stock_price
+
+--
+
+SELECT MAX(close - open)
+  FROM tutorial.aapl_historical_stock_price
+
+--
+
+SELECT AVG(volume) AS avg_volume
+  FROM tutorial.aapl_historical_stock_price
+
+--
+
+SELECT year,
+       month,
+       SUM(volume) AS volume_sum
+  FROM tutorial.aapl_historical_stock_price
+ GROUP BY year, month
+ ORDER BY year, month
+
+--
+
+SELECT year,
+       AVG(close - open) AS avg_daily_change
+  FROM tutorial.aapl_historical_stock_price
+ GROUP BY 1
+ ORDER BY 1
+
+--
+
+SELECT year,
+       month,
+       MIN(low) AS lowest_price,
+       MAX(high) AS highest_price
+  FROM tutorial.aapl_historical_stock_price
+ GROUP BY 1, 2
+ ORDER BY 1, 2
+
+--
+
+SELECT player_name,
+       state,
+       CASE WHEN state = 'CA' THEN 'yes'
+            ELSE NULL END AS from_california
+  FROM benn.college_football_players
+ ORDER BY 3
+
+--
+
+SELECT player_name,
+       height,
+       CASE WHEN height > 74 THEN 'over 74'
+            WHEN height > 72 AND height <= 74 THEN '73-74'
+            WHEN height > 70 AND height <= 72 THEN '71-72'
+            ELSE 'under 70' END AS height_group
+  FROM benn.college_football_players
+
+--
+
+SELECT *,
+       CASE WHEN year IN ('JR', 'SR') THEN player_name ELSE NULL END AS upperclass_player_name
+  FROM benn.college_football_players
+
+--
+
+SELECT CASE WHEN state IN ('CA', 'OR', 'WA') THEN 'West Coast'
+            WHEN state = 'TX' THEN 'Texas'
+            ELSE 'Other' END AS arbitrary_regional_designation,
+            COUNT(1) AS players
+  FROM benn.college_football_players
+ WHERE weight >= 300
+ GROUP BY 1
+
+--
+
+SELECT CASE WHEN year IN ('FR', 'SO') THEN 'underclass'
+            WHEN year IN ('JR', 'SR') THEN 'upperclass'
+            ELSE NULL END AS class_group,
+       SUM(weight) AS combined_player_weight
+  FROM benn.college_football_players
+ WHERE state = 'CA'
+ GROUP BY 1
+
+--
+
+SELECT state,
+       COUNT(CASE WHEN year = 'FR' THEN 1 ELSE NULL END) AS fr_count,
+       COUNT(CASE WHEN year = 'SO' THEN 1 ELSE NULL END) AS so_count,
+       COUNT(CASE WHEN year = 'JR' THEN 1 ELSE NULL END) AS jr_count,
+       COUNT(CASE WHEN year = 'SR' THEN 1 ELSE NULL END) AS sr_count,
+       COUNT(1) AS total_players
+  FROM benn.college_football_players
+ GROUP BY state
+ ORDER BY total_players DESC
+
+--
+
+SELECT CASE WHEN school_name < 'n' THEN 'A-M'
+            WHEN school_name >= 'n' THEN 'N-Z'
+            ELSE NULL END AS school_name_group,
+       COUNT(1) AS players
+  FROM benn.college_football_players
+ GROUP BY 1
+
+--
+
